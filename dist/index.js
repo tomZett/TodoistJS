8e2cf903c8c3cf23a9ba73de097815c9b071bb9c
tom zett <tzirnsack@web.de>
2018-01-29 21:26:04 +0100

    First commonjs module tests.

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TodoistNodeAPI = /** @class */ (function () {
+    function TodoistNodeAPI() {
+    }
+    return TodoistNodeAPI;
+}());
+exports.TodoistNodeAPI = TodoistNodeAPI;
